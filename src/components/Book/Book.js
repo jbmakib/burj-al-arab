7389f552b9106ec0a9fedb2d9da6b9e113240eef
jbmakib <mdfoujolmobin@gmail.com>
2021-10-11 20:55:47 +0600

    book component updated for single route

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 const Book = () => {
-    const {bedType} = useParams();
+    const { bedType } = useParams();
     return (
-        <div style={{textAlign: 'center'}}>
-            <h1>Let's book a {bedType} Room.</h1>
-            <p>Want a <Link to="/home">different room?</Link> </p>
+        <div style={{ textAlign: "center" }}>
+            {bedType ? (
+                <>
+                    <h1>Let's book a {bedType} Room.</h1>
+                    <p>
+                        Want a <Link to="/home">different room?</Link>
+                    </p>
+                </>
+            ) : (
+                <>
+                    <h1>Welcome to book</h1>
+                    <p>
+                        Want a <Link to="/home">room?</Link>
+                    </p>
+                </>
+            )}
         </div>
     );
 };
